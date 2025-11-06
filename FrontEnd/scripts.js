159992fe26eb2c4ee9d57549f2c3ccd9a18b5a20
Sylvie Trolle <sylvie.trolle@gustaveroussy.fr>
2025-11-06 15:40:59 +0100

    Ajout dynamiquement des boutons filtres

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -1,3 +1,5 @@
+// Récupération et affichage des projets sur la page d'Accueil
+
 const reponse = await fetch("http://localhost:5678/api/works");
 const projets = await reponse.json();
 
@@ -18,7 +20,37 @@ projets.forEach((projet) => {
   galerie.appendChild(figure);
 });
 
-console.log(
-  "Projets récupérés :",
-  projets.map((p) => p.title)
-);
+// Ajout dynamique des 4 boutons de filtres
+
+const divFiltres = document.createElement("div");
+divFiltres.classList.add("filtres");
+
+const btnFiltreTous = document.createElement("button");
+btnFiltreTous.textContent = "Tous";
+btnFiltreTous.classList.add("filtre-tous");
+
+const btnFiltreObjets = document.createElement("button");
+btnFiltreObjets.textContent = "Objets";
+btnFiltreObjets.classList.add("filtre-objets");
+
+const btnFiltreAppartements = document.createElement("button");
+btnFiltreAppartements.textContent = "Appartements";
+btnFiltreAppartements.classList.add("filtre-appartements");
+
+const btnFiltreHotelRestaurant = document.createElement("button");
+btnFiltreHotelRestaurant.textContent = "Hôtels & restaurants";
+btnFiltreHotelRestaurant.classList.add("filtre-hotels-restaurants");
+
+divFiltres.appendChild(btnFiltreTous);
+divFiltres.appendChild(btnFiltreObjets);
+divFiltres.appendChild(btnFiltreAppartements);
+divFiltres.appendChild(btnFiltreHotelRestaurant);
+
+const portfolio = document.getElementById("portfolio");
+
+portfolio.insertBefore(divFiltres, galerie);
+
+// Récupération via l'API des catégories des projets
+
+const reponseCategories = await fetch("http://localhost:5678/api/categories");
+const categories = await reponseCategories.json();
